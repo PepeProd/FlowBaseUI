eeec5fcaec96d1a4a9d7f5bf8fde77d15d1e9c1f
Kiril <peterpep@umich.edu>
2017-12-09 13:12:06 -0500

    add expired and expiring soon list to overview

--- src/pages/ChemicalOverview.vue
+++ src/pages/ChemicalOverview.vue
@@ -1,6 +1,42 @@
 <template>
     <div class="chemicalOverview">
-        <h2>Chemical Inventory</h2>
+        <h1>Chemical Inventory</h1>
+        <div class="listContainer">
+            <label class="listTitle">Expired Chemicals</label>
+            <img class="iconDisplay" :class="{iconDisplayActive : this.displayExpired}" src="../assets/chevron_blue.svg" 
+                @click="displayExpired = !displayExpired"/>
+            <div v-show="this.displayExpired" >
+                <div v-for="chem in getExpiredChemicals">
+                    <div style="margin-top: 10px; ">
+                        <label style="text-decoration: underline;">{{chem["chemical_name"]}}</label>
+                        <div> 
+                            <label class="listBarcode">Barcode: {{chem["barcode"]}}</label>
+                        </div>
+                        <div>
+                            <label class="listExpire">Expired On: {{chem["expiration_date"]}}</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div :class="{spacingSmall: !this.displayExpired, spacingBig: this.displayExpired}"  class="listContainer">
+            <label class="listTitle">Expiring Soon Chemicals</label>
+            <img class="iconDisplay" :class="{iconDisplayActive : this.displayExpiringSoon}" src="../assets/chevron_blue.svg" 
+                @click="displayExpiringSoon = !displayExpiringSoon"/>
+            <div v-show="this.displayExpiringSoon">
+                <div v-for="chem in getExpiringSoonChemicals">
+                    <div style="margin-top: 10px; ">
+                        <label style="text-decoration: underline;">{{chem["chemical_name"]}}</label>
+                        <div> 
+                            <label class="listBarcode">Barcode: {{chem["barcode"]}}</label>
+                        </div>
+                        <div>
+                            <label class="listExpire">Expired On: {{chem["expiration_date"]}}</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <TableSearcher class="tableSearch" :rows="chemicalsWithNoDuplicates" :columnNames="this.columnsForSearcher" @submitClicked="handleSubmitClicked"></TableSearcher> 
         <div v-for="chemical in chemicalFamiliesReactiveGetter" class="chemFamilies">
             <div class="noStyleLink">
@@ -17,15 +53,19 @@
 
 <script>
     import TableSearcher from '../components/TableSearcher.vue';
+    import {dateComparison} from '../mixins/dateComparison.js';
     export default {
         name: "Overview",
         components: {
             TableSearcher
         },
+        mixins: [dateComparison],
         data() {
             return {
                 chemicalFamilies: [],
-                columnsForSearcher: ['name', 'quantity']
+                columnsForSearcher: ['name', 'quantity'],
+                displayExpired: false,
+                displayExpiringSoon: false,
             }
         },     
         computed: {
@@ -46,6 +86,28 @@
             },
             chemicalFamiliesReactiveGetter: function() {
                 return this.chemicalFamilies;
+            },
+            getExpiredChemicals: function() {
+                var items = this.$store.getters.chemicals;
+                var expiredChemicals = [];
+                
+                for (var item, i=0; item=items[i++];) {
+                    if (this.compareExpired(item["expiration_date"])) {
+                        expiredChemicals.push(item);
+                    }                    
+                } 
+                return expiredChemicals;
+            },
+            getExpiringSoonChemicals: function() {
+                var items = this.$store.getters.chemicals;
+                var expiringSoonChemicals = [];
+
+                for (var item, i=0; item=items[i++];) {
+                    if (this.compareSoonExpiration(item["expiration_date"])) {
+                        expiringSoonChemicals.push(item);
+                    }                    
+                } 
+                return expiringSoonChemicals;
             }
         },
         methods: {
@@ -93,6 +155,26 @@
     cursor: pointer;
 }
 img {
+}
+
+.listBarcode, .listExpire{
+    font-weight: normal;
+    font-size: 10pt;
+}
+.listContainer > div {
+    font-size: 11pt;
+}
+.listTitle {
+    font-size: 14pt;
+    font-weight: 600;
+}
+
+.spacingSmall {
+    margin-top: 5px;
+}
+
+.spacingBig {
+    margin-top: 25px;
 }
 
 .iconDisplayActive {
